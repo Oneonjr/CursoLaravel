47e562e29438b8639ac1b5730c3f81070e23fd0f
Oneonjr <oneonjunior@outlook.com>
2024-09-26 11:56:10 -0300

    Finalizando Curso

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\SeriesController;
+// use App\Http\Controllers\Api\SeriesController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/series',\App\Http\Controllers\Api\SeriesController::class);
+// Route::apiResource('/series',SeriesController::class);
+Route::get('/series',[\App\Http\Controllers\Api\SeriesController::class, 'index']);
 
 
 // Route::get('/series',[\App\Http\Api\SeriesController::class, 'index']);
